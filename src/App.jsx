06d81bd751866a2dfe8b51959adbc06c129c3b52
Sam-Panna <sampannayonjan@gmail.com>
2025-01-05 14:59:15 +0545

    OnChange

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,24 @@ const App = () => {
 
   //UseState
 
-  const[click, setClick] = useState(true)
-  
-  const handleClick = () => {
-    setClick(!click);
-    click ? document.body.style.backgroundColor = "black" : document.body.style.backgroundColor = "white" 
-    console.log(click);
-    
+  // const[click, setClick] = useState(true)
+
+  // const handleClick = () => {
+  //   setClick(!click);
+  //   click ? document.body.style.backgroundColor = "black" : document.body.style.backgroundColor = "white" 
+  //   console.log(click);
+
+  // }
+
+  const [text, setText] = useState('')
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+
+    console.log(text);
   }
 
-  
+
 
   // const [count, setCount] = useState(0)
 
@@ -27,29 +35,38 @@ const App = () => {
 
   // }
   // console.log(count);
-  
+
   return (
 
     <>
 
 
-    {/* <Product/> */}
-    {/* <Content/> */}
+      {/* <Product/> */}
+      {/* <Content/> */}
 
-    <div className="w-full flex items-center gap-4 justify-center">
+      {/* <div className="w-full flex items-center gap-4 justify-center">
       
       <button onClick={handleClick} className="p-2 px-4 bg-blue-500 rounded-md">{click ? "ON" : "OFF"}</button>
-    </div>
+    </div> */}
+      <form>
 
-    
-    
-    
-    
-   
+        <div className="h-[100vh] w-full flex items-center gap-4 justify-center flex-col ">
+          <input type="text" onChange={handleChange}  className="border-2 border-black rounded-md" />
+          <input type="text" className="border-2 border-black rounded-md" value={text} />
+
+
+        </div>
+
+      </form>
+
+
+
+
+
     </>
-  
+
   )
-   
+
 }
 
 export default App
